cbe288d7040dee9de17958eedf8a69ee015fe126
Kylian Schwaiger <schwaiger.lehartel.kylian@gmail.com>
2026-03-24 10:03:15 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == "exit":
+            break
         print(f"{command}: command not found")
-    pass
-
 
 if __name__ == "__main__":
     main()
