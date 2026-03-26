f1de1506f07c5ec7fce0f7d682b3360a41944be7
Kylian Schwaiger <schwaiger.lehartel.kylian@gmail.com>
2026-03-26 23:54:09 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 builtins = ["exit", "type", "echo"]
 
@@ -18,8 +19,17 @@ def main():
 
             if cmd in builtins:
                 print(f"{cmd} is a shell builtin")
+
             else:
-                print(f"{cmd}: not found")
+                for directory in os.environ.get("PATH").split(os.pathsep):
+                     full_path = os.path.join(directory, cmd)
+
+                     if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                        print(f"{cmd} is {full_path}")
+                        break
+
+                else:
+                    print(f"{cmd}: not found")
         
         else:
             print(f"{command}: not found")
