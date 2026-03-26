234da0cada6725204c2f4656e46b238db6ea885d
Kylian Schwaiger <schwaiger.lehartel.kylian@gmail.com>
2026-03-26 16:20:35 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,28 @@
 import sys
 
+builtins = ["exit", "type", "echo"]
+
 def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+
         if command == "exit":
             break
+
         if command.startswith("echo"):
             print(command[5:])
-        else:
-            print(f"{command}: command not found")
+
+        if command.startswith("type"):
+            cmd = command[5:].strip()
+
+            if cmd in builtins:
+                print(f"{cmd} is a shell builtin")
+            else:
+                print(f"{cmd}: not found")
+
+
+
 
 if __name__ == "__main__":
     main()
